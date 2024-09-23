d978b98086b7a54fd0f47b6d525a8e01214b8313
mayrabpi <145108717+mayrabpi@users.noreply.github.com>
2024-09-23 20:00:29 +0200

    mas

--- 12_sumatorio.py
+++ 12_sumatorio.py
@@ -1,5 +1,6 @@
 numero = int(input("Ingrese un número"))
 suma = 0
-for i in range(1,numero+1):
+for i in range(numero+1):
     suma+=i
 print("La suma de los números de 1 a ", numero, " es ", suma)
+
--- 18_lista_ordenada.py
+++ 18_lista_ordenada.py
@@ -5,3 +5,7 @@ print("Lista ordenada" ,numeros)
 numeros.reverse()
 print("Lista ordenada inversa" ,numeros)
 
+numeros = [5, 2, 9, 1, 5, 6]
+
+print("Lista ordenada en forma ascendente: ", sorted(numeros))
+print("Lista ordenada en forma descendente: ", sorted(numeros, reverse=True))
